eb2f8f775e8d82ebb29162ccc744b67a0b65a674
Vijay Jaiswal <vijay.jaiswal@qio.io>
2021-08-23 23:08:12 +0530

    Added Assignment 1 - Q1 Code

---
+++ Assignment-1/BDSAssignment1-1/src/test/java/com/bits/wilp/bds/assignment1/BdsAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.wilp.bds.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdsAssignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

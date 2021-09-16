22271f7b7fa30d73f990df79249c2da5ca1a0aa6
Vijay Jaiswal <vijay.jaiswal@qio.io>
2021-09-16 12:42:53 +0530

    Update the loader for adding Index and created generic methods for query

--- Assignment-2/data_loader.py
+++ Assignment-2/data_loader.py
@@ -1,27 +1,65 @@
-from pymongo import MongoClient
-import numpy as np
+# Part 1: Data loader application
 
-client = MongoClient()
+# Write a data loader application to do the following:
+# 1.	Read the records and insert them into a MongoDB collection but with the transformations specified below.
+#       Choose appropriate data types.
+# 2.	There may be duplicate records in the data set. Delete duplicates during insertion.
+# 3.	Remove the URL field during insertion.
+# 4.	Combine the outcode and postcode into one zip code field in the DB, i.e. zip code = outcode + postcode.
+# 5.	Create any indices that you think may be important based on the query types specified in the next part.
+import pymongo
+from pymongo import MongoClient
+import pandas as pd
+
+# Creating Mongodb connectivity to "rest" database
+client = MongoClient('localhost', 27017)
+# Database Name : rest
 db = client.rest
+# Collection Name : rest
 rest = db.rest
 
-import pandas as pd
+# Reading "res.json" file to be loaded into the Mongodb
 df=pd.read_json('res.json', lines="true")
 
+# Transformation as per detail above
 print("Performing Data transformations")
+
 df['_id'] = [a['$oid'] for a in df['_id']]
-del df['URL']
+
+# Creating ZipCode using outcode and postcode
 df['zipcode'] = df['outcode']+df['postcode']
+
+# Deleting NOT required attributes URL, postcodeand outcode
+del df['URL']
 del df['postcode']
 del df['outcode']
+
+# Updating rating is the are missing and converting it into float64 type
 df['rating'] = df['rating'].apply(lambda x: 0 if x == 'Not yet rated' else x )
+
+# Dropping Duplicate records
 df = df.drop_duplicates(subset=["address", "name", "zipcode", "rating", "type_of_food"])
 
+
 print("After Data transformations")
+print(df.info())
 print(df.head())
 
 data_dict = df.to_dict('records')
 
 print("Saving to Mongo DB")
+# Purging collection before inserting new transformed data
 rest.delete_many({})
 rest.insert_many(data_dict)
+
+# Number of documents in neighborhoods collection
+records_inserted=rest.count_documents({})
+print(records_inserted," records inserted successfully")
+
+# Creating Index on type_of_food, zipcode, address
+print("\nCreating Required index.")
+index_response= rest.create_index([("type_of_food", pymongo.DESCENDING),
+          ("zipcode", pymongo.ASCENDING),
+          ("address", pymongo.ASCENDING)], name="ind_food_zip_address")
+print("index created with name:", index_response)
+print(rest.index_information())
--- Assignment-2/data_query.py
+++ Assignment-2/data_query.py
@@ -1,19 +1,35 @@
-from PyInquirer import prompt
-from examples import custom_style_2
+# Part 2: Data query application
+
+# Write an end user facing query application which can run the following query types after taking required user inputs.
+# Only specified output fields need to be returned.
+# 1.	User provides a choice of cuisine and zipcode, the query returns a list of top 5 restaurant names with full
+#       address and rating with results sorted descending order based on rating.
+# 2.	User provides a string to be searched in the address field and a minimum rating. The query returns all
+#       restaurant names and cuisine that match the inputs provided along with the match score.
+#       Sort output by descending score.
+# 3.	Give a cuisine, output how many (i.e. count) matching restaurants are there per zip code.
+#       Sort descending the 2 column output (zipcode, count) by the count.
+# 4.	Show the average rating per type of food and provide an ascending sorted output (type of food, rating) by rating.
+#
+# You can create the data query application with the 4 types of query types encoded as functions.
+# The user can run this program from the command line with parameters : type of query (1-4) and corresponding inputs.
+# The query is executed on the DB and results returned on console.
+
 from prompt_toolkit.validation import Validator, ValidationError
 
 from pymongo import MongoClient
 from bson.son import SON
 
-import numpy as np
-
-client = MongoClient()
+# Creating Mongodb connectivity to "rest" database
+client = MongoClient('localhost', 27017)
+# Database Name : rest
 db = client.rest
+# Collection Name : rest
 rest = db.rest
 
-
+# Generic class for Number Validation
 class NumberValidator(Validator):
-
+    # Function to validate text is a number
     def validate(self, document):
         try:
             int(document.text)
@@ -21,9 +37,7 @@ class NumberValidator(Validator):
             raise ValidationError(message="Please enter a number",
                                   cursor_position=len(document.text))
 
-
-
-
+# Banner for User Input
 questions = [
     {
         'type': 'list',
@@ -31,89 +45,107 @@ questions = [
         'message': 'Welcome to MongoDB Data Query',
         'choices': ["Q1","Q2","Q3", "Q4"]
     }
-
-    
-
-
 ]
 
+# Generic method to display unique values of an attribute of a collection
+def get_unique_values(collection, attribute):
+    attributes = "Possible "+attribute+":"
+    for values in collection.distinct(attribute):
+        attributes = attributes + "," + str(values)
+    print(attributes)
+
+# Generic method to execute Mongodb find query on a collection
+def execute_query(collection, myquery,fields, sort_col, sort_col_order, limits):
+    if fields!="":
+        mydoc = collection.find(myquery, fields).sort(sort_col, sort_col_order).limit(limits)
+    else:
+        mydoc = collection.find(myquery).sort(sort_col, sort_col_order).limit(limits)
+
+    print("Executing: ",myquery,"\nQuery Output: ")
+    count=collection.count_documents(myquery)
+    if count!= 0:
+        print("Found", count, "documents matching the query :) (displaying only ", limits, ")")
+        for x in mydoc:
+            print(x)
+    else:
+        print("No record found for query! :( ")
+
+# Generic method to execute Mongodb aggregate query on a collection
+def execute_query_aggregate(collection, myquery, groupBy, sort_col, sort_col_order, limits):
+    agg_query=[
+        {"$match": myquery},
+        {"$group": groupBy},
+        {"$sort": {sort_col: sort_col_order}},
+        {"$limit": limits}
+    ]
+    mydoc = rest.aggregate(agg_query);
+
+    print("Executing: ",agg_query,"\nQuery Output: ")
+    count=collection.count_documents(myquery)
+    if count!=0:
+        print("Found",count,"documents matching the query :) (displaying only ",limits,")")
+        for x in mydoc:
+            print(x)
+    else:
+        print("No record found for query! :( ")
+
+# Method for Question 1
 def q1():
     print("\nQuestion 1:")
     print("User provides a choice of cuisine and zipcode, the query returns a list of \ntop 5 restaurant names with full address and rating with results sorted descending order based on rating.")
     print("\nEnter Zip code (example : CF243JH)")
+    get_unique_values(rest, 'zipcode')
+
     zc = input()
     print("Enter Type of food (example : Lebanese)")
+    get_unique_values(rest, 'type_of_food')
     tf = input()
 
     # Sample Input - "CF243JH", "Lebanese"
-
     myquery = { "zipcode": zc, "type_of_food": tf}
+    fields = ""
+    execute_query(rest, myquery,fields, "rating", -1, 10)
 
-    mydoc = rest.find(myquery).sort("rating", -1).limit(5)
-    print("\nResult: ")
-    for x in mydoc:
-        print(x)
-
+# Method for Question 2
 def q2():
     print("\nQuestion 2:")
     print("User provides a string to be searched in the address field and a minimum rating. \nThe query returns all restaurant names and cuisine that match the inputs provided along with the match score. \nSort output by descending score.")
     print("\nEnter Address (example : City)")
+    get_unique_values(rest, 'address')
     ad = input()
     print("Enter minimum rating (example : 4)")
+    get_unique_values(rest, 'rating')
     mr = int(input())
 
     #Sample Input City, 4
 
     myquery = {"address": { "$regex": ad}, "rating": { "$gt": mr} }
-    fields = {'name': 1, 'type_of_food': 1}
+    fields = {'name': 1, 'type_of_food': 1, 'address':1, 'rating':1 }
+    execute_query(rest, myquery, fields, "rating", -1, 10)
 
-    # TODO: How to identify match score
-    mydoc = rest.find(myquery, fields).sort("rating", -1)
-    print("\nResult: ")
-    for x in mydoc:
-        print(x)
-
+# Method for Question 3
 def q3() :
     print("\nQuestion 3:")
     print("Give a cuisine, output how many (i.e. count) matching restaurants are there per zip code. \nSort descending the 2 column output (zipcode, count) by the count.")
     print("\nEnter Type of food (example: Lebanese)")
+    get_unique_values(rest, 'type_of_food')
     tf = input()
-    myquery = {"type_of_food": tf}
-    mydoc = rest.aggregate([
-        {"$match":myquery},
-        {"$group":{"_id":{"ZIPCODE":"$zipcode"},
-                "Count":{"$sum":1}}},
-        {"$sort":SON([("Count", -1)])}
-    ]);
-    print("\nResult: ")
-    for x in mydoc:
-        print(x)
 
+    myquery = {"type_of_food": tf}
+    groupBy ={"_id":{"ZIPCODE":"$zipcode","type_of_food":"$type_of_food"}, "Count":{"$sum":1}}
+    execute_query_aggregate(rest, myquery, groupBy,"Count",-1, 10)
+
+# Method for Question 4
 def q4() :
     print("\nQuestion 4:")
     print("Show the average rating per type of food and provide an ascending sorted output \n(type of food, rating) by rating.")
-    mydoc = rest.aggregate([
-        {"$group":{"_id":{"Type Of Food":"$type_of_food"},
-                "AVG_RATING":{"$avg":"$rating"}}},
-        {"$sort":SON([("AVG_RATING", -1)])}
-    ]);
-    print("\nResult: ")
-    for x in mydoc:
-        print(x)
 
-def add(a, b):
-    print(a + b)
-
-def difference(a, b):
-    print(a - b)
-
-def product(a, b):
-    print(a * b)
-
-def divide(a, b):
-    print(a / b)
+    myquery = {}
+    groupBy = {"_id":{"Type Of Food":"$type_of_food"},"AVG_RATING":{"$avg":"$rating"}}
+    execute_query_aggregate(rest, myquery, groupBy, "AVG_RATING", -1, 10)
 
 
+# Main Method to Run the application and get user's input
 def main():
     print("##############################################################################")
     print("Welcome to MongoDB Data Query")
